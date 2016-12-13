ba0d28623bed8d40423a85a0d13ca3e9a7e4a32c
PikalovaM <pandochka94@yandex.ru>
2016-12-13 20:59:43 +0300

    Add a new module to indificate neutron flying the wrong side

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -13,5 +13,18 @@ int main()
 {
 	default_random_engine generator (seed);
     uniform_real_distribution<double> udis(0,1); // функция для генерации СВ из равномерного распределения
-	
+	for(int i=1; i<=M; i++)
+    {
+
+        x0 = 0;
+        event = false;
+        begin = true;
+        while(!event) //пока нейтрон не поглотится или не рассеятся
+        {
+            alpha = angle_dis(generator);
+            if (begin and alpha>M_PI/2)			//если нейтрон полетит не в ту сторону
+            {
+                i--;
+                break;
+            } 
 }
