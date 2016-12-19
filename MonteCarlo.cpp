7f7250fb5800971b907787b22f85f32840d35b22
LugovayaDaria <amdalug@gmail.com>
2016-12-19 17:45:53 +0300

    Added functions, if the neutron doesn't pass and pass through the barrier

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -19,12 +19,22 @@ int main()
         x0 = 0;
         event = false;
         begin = true;
-        while(!event) //пока нейтрон не поглотится или не рассеятся
-        {
-            alpha = angle_dis(generator);
-            if (begin and alpha>M_PI/2)			//если нейтрон полетит не в ту сторону
-            {
-                i--;
-                break;
-            } 
-}
+		while (!event) //пока нейтрон не поглотится или не рассеятся
+		{
+			alpha = angle_dis(generator);
+			if (begin and alpha > M_PI / 2)			//если нейтрон полетит не в ту сторону
+			{
+				i--;
+				break;
+			}
+			x0_s = GenerateExponential(sigma_a + sigma_s, udis(generator));//СВ из экспоненциального распределения
+			if (x0 + x0_s*cos(alpha) < 0) //если не прошёл сквозь преграду, продолжаем дальше
+				continue;
+			if (x0 + x0_s*cos(alpha) >= L) //нейтрон прошёл сквозь преграду
+			{
+				N++;
+				event = true;
+				cout << "Passed, N=" << N << endl;
+				break;
+			}
+		}
